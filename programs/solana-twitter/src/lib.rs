ab60f4bc68d1c43a6caae5196e089b5e34d2d8d1
Leo Donoso <leonardoadonosog@gmail.com>
2022-04-13 22:35:46 -0400

    Frontend Scaffolded

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -2,14 +2,78 @@ use anchor_lang::prelude::*;
 
 declare_id!("Cmx8tcBfPpmv6vJ1sqonPTHESYJpnTSgS8K7DD1TfpfM");
 
+// Defining error codes
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
+
+// Main instruction
+
 #[program]
 pub mod solana_twitter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get()?;
+
+        if topic.chars().count() > 50 {
+            return Err(error!(ErrorCode::TopicTooLong));
+        }
+
+        if content.chars().count() > 280 {
+            return Err(error!(ErrorCode::ContentTooLong));
+        }
+
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Define structure of the tweet.
+
+#[account] 
+pub struct Tweet {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+// Add some useful constants for sizing properties.
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 200;
+const MAX_CONTENT_LENGTH: usize = 1120;
+
+// Add constant of tweet account that provides it's total size
+
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
